0c10efe12216fc11159e4e616718f7b84a0dde0b
Gurukiran M <gurukiran2504@gmail.com>
2025-03-14 20:32:58 +0530

    Update page.tsx

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -8,8 +8,12 @@ import Image from "next/image";
 import Link from "next/link";
 import { redirect} from "next/navigation";
 
+// type Props = {
+//   params: { id: string };
+// };
+
 type Props = {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 };
 
 const ProductDetails = async ({ params }: Props) => {
